9bee68d57eea6e5c9af45d00ecd082c638200daf
lenzarata93 <117170826+lenzarata93@users.noreply.github.com>
2024-07-08 11:20:40 +0300

    create components
    create components for Summary and List of expenses

---
+++ ExpenseTrackerApp/components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,10 @@
+import { View , Text } from "react-native";
+function ExpensesSummary() {
+  return (
+    <View>
+      <Text>Last 7 days</Text>
+      <Text>$182.34</Text>
+    </View>
+  );
+}
+export default ExpensesSummary;

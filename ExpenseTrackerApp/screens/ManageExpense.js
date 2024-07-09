c653a601254a450e1f635c7316c266d6fb2365f9
lenzarata93 <117170826+lenzarata93@users.noreply.github.com>
2024-07-09 10:28:32 +0300

    Update ManageExpense.js
    adding delete button

--- ExpenseTrackerApp/screens/ManageExpense.js
+++ ExpenseTrackerApp/screens/ManageExpense.js
@@ -1,5 +1,7 @@
 import { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { StyleSheet, View } from "react-native";
+import IconButton from "../components/UI/iconButton";
+import { GlobalStyles } from "../constants/styles";
 function ManageExpenses({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
 
@@ -10,6 +12,34 @@ function ManageExpenses({ route, navigation }) {
     });
   }, [navigation, isEditing]);
 
-  return <Text>MANAGE EXPENSES SCREEN</Text>;
+  function deleteExpenseHandler() {}
+  return (
+    <View style={styles.container}>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={28}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
 export default ManageExpenses;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 22,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
